9a84ca2b180ac96d74d4a71ad4cb00f183ca3946
mihir <mihir@gmail.com>
2023-12-26 00:58:35 +0530

    all clear

---
+++ html and css/portfolio.js
@@ -0,0 +1,12 @@
+const text = document.querySelector(".second-text");
+        
+            const textLoad = () =>  {
+                    setTimeout(() => {
+                        text.textContent = "Mihir";
+                    }, 0);
+                    setTimeout(() => {
+                        text.textContent = "Web Developer";
+                    }, 4300);
+            }
+            textLoad();
+            setInterval(textLoad, 8000);
