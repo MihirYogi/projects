c2c2455e3d55d65b8cbf049dea73ca2b6166c346
mihir <mihir@gmail.com>
2023-12-28 00:25:59 +0530

    navigation bar sticky completed and 2 page image editing completed

--- html and css/portfolio.js
+++ html and css/portfolio.js
@@ -15,4 +15,9 @@ const text = document.querySelector(".second-text");
             setInterval(textLoad, 8000);
 
 
-// navigation sticky scrolling
+// responsive navigation bar 
+
+const header = document.querySelector("header");
+            window.addEventListener("scroll",function(){
+                header.classList.toggle("sticky", window.scrollY > 120);
+            });
