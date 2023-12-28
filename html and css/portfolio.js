751e93d4b647c3973891e9c20e91b7908fb053fc
mihir <mihir@gmail.com>
2023-12-29 02:23:49 +0530

    created scrolling animation for image and textgit status

--- html and css/portfolio.js
+++ html and css/portfolio.js
@@ -21,3 +21,22 @@ const header = document.querySelector("header");
             window.addEventListener("scroll",function(){
                 header.classList.toggle("sticky", window.scrollY > 120);
             });
+
+
+//scroll animation
+
+let sections = document.querySelectorAll(".ani");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show-animate');
+        } else {
+            entry.target.classList.remove('show-animate');
+        }
+    });
+}, { threshold: 0.5 }); 
+
+sections.forEach(section => {
+    observer.observe(section);
+});
